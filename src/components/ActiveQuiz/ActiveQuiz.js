df2f69c88a590dd869b4c68105aa17fc351a4e0f
dim2a <buinicki.dim@gmail.com>
2018-11-14 14:18:12 +0200

    Создан список вопросов

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './ActiveQuiz.css'
+import AnswersList from './AnswersList/AnswersList'
 
 const ActiveQuiz = props => (
     <div className={classes.ActiveQuiz}>
@@ -12,13 +13,9 @@ const ActiveQuiz = props => (
             </span>
             <small>4 из 12</small>
         </p>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-        </ul>
+        <AnswersList 
+            answers={props.answers}
+        />
     </div>
 )
 
