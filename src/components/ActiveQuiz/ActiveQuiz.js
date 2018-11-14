e0f5b6cbb7a866a955376ade9e6124ad7e5d3962
dim2a <buinicki.dim@gmail.com>
2018-11-14 14:39:36 +0200

    добавлена обработка выбора варианта ответа

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -9,12 +9,13 @@ const ActiveQuiz = props => (
                 <strong>
                     2.
                 </strong>
-                Как дела?
+                {props.question}
             </span>
             <small>4 из 12</small>
         </p>
         <AnswersList 
             answers={props.answers}
+            onAnswerClick={props.onAnswerClick}
         />
     </div>
 )
