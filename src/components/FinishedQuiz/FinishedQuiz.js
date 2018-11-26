44b61368cc520ca9dec311ea72bc0165181aea5b
dim2a <buinicki.dim@gmail.com>
2018-11-26 15:49:27 +0200

    Создан кампонент кнопки

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './FinishedQuiz.css'
+import Button from '../UI/Button/Button'
 
 const FinishedQuiz = props => {
     const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -33,7 +34,12 @@ const FinishedQuiz = props => {
             </ul>
             <p>Правильно {successCount} из {props.quiz.length}</p>
             <div>
-                <button onClick={props.onRetry}>Повторить</button>
+                <Button onClick={props.onRetry} type='primary'>
+                    Повторить
+                </Button>
+                <Button type='success'>
+                    Перейти в список текстов
+                </Button>
             </div>
         </div>
     )
