4fcab37df1a17a5df018d3a83045aae7da759fde
dim2a <buinicki.dim@gmail.com>
2018-11-30 14:46:10 +0200

    Реализован компонент страницы поиска

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,10 +1,34 @@
 import React, { PureComponent } from 'react'
+import classes from './QuizList.css'
+import {NavLink} from 'react-router-dom'
 
 export default class QuizList extends PureComponent {
-  render() {
+  
+    renderQuizes() {
+        return[1,2,3].map((quiz, index) => {
+            return (
+                <li
+                    key={index}
+                >
+                  <NavLink to={'/quiz/'+ quiz}>
+                      Тест {quiz}
+                  </NavLink>
+                </li>
+            )
+        })
+    }
+  
+    render() {
     return (
-      <div>
-        <h1>QuizList</h1>
+      <div className={classes.QuizList}>
+        <div>
+            <h1>Список Тестов</h1>
+
+            <ul>
+                {this.renderQuizes()}
+            </ul>
+        </div>
+        
       </div>
     )
   }
