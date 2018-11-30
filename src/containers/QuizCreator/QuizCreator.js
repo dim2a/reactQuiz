e29e0ebb0f079a05a8cd0a27bdf4598eb3fbadea
dim2a <buinicki.dim@gmail.com>
2018-11-30 14:06:05 +0200

    Добавлен роутинг и навигация

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class QuizCreator extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>QuizCreator</h1>
+      </div>
+    )
+  }
+}
---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class QuizList extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>QuizList</h1>
+      </div>
+    )
+  }
+}
