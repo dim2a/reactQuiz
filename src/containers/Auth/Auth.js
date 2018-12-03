9a5cea15510b5018c10851b6aafbd4280e68a2a9
dim2a <buinicki.dim@gmail.com>
2018-12-03 16:32:09 +0200

    Создана страница и форма авторизации, создан универсальный инпут

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,53 @@
 import React, { PureComponent } from 'react'
+import classes from './Auth.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends PureComponent {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+    
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form className={classes.AuthForm} onSubmit={this.submitHandler}>
+            <Input 
+              label="Email" 
+            />
+
+            <Input 
+              label="Пароль" 
+              errorMessage={'TEST'}
+            />
+
+            <Button 
+            type="success"
+            onClick={this.loginHandler}
+            >
+              Войти
+            </Button>
+            <Button 
+            type="primary"
+            onClick={this.registerHandler}
+            >
+              Регистрация
+            </Button>
+          </form>
+
+        </div>
       </div>
     )
   }
