d8e8b4ba4299785399b1db9d8d1d1f873d997ffa
dim2a <buinicki.dim@gmail.com>
2018-12-11 15:50:30 +0300

    Доработана загрузка теста

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,39 +1,62 @@
 import React, { PureComponent } from 'react'
 import classes from './QuizList.css'
 import {NavLink} from 'react-router-dom'
-import axios from 'axios'
+import axios from '../../axios/axios-quiz'
+import Loader from '../../components/UI/Loader/Loader';
 
 export default class QuizList extends PureComponent {
+
+    state = {
+        quizes: [],
+        loading: true
+    }
   
     renderQuizes() {
-        return[1,2,3].map((quiz, index) => {
+        return this.state.quizes.map( quiz => {
             return (
                 <li
-                    key={index}
+                    key={quiz.id}
                 >
-                  <NavLink to={'/quiz/'+ quiz}>
-                      Тест {quiz}
+                  <NavLink to={'/quiz/'+ quiz.id}>
+                    {quiz.name}
                   </NavLink>
                 </li>
             )
         })
     }
 
-    componentDidMount() {
-        axios.get('https://react-quiz-b4314.firebaseio.com/quiz.json').then(response => {
-          console.log(response)
-        })
+    async componentDidMount() {
+        try {
+            const response = await axios.get('/quizes.json')
+            
+            const quizes = []
+
+            Object.keys(response.data).forEach((key, index) => {
+                quizes.push({
+                    id: key,
+                    name: `Тест № ${index + 1}`
+                })
+            })
+            this.setState({
+                quizes, loading: false
+            })
+        } catch (e) {
+            console.log(e)
+        }
     }
-  
+
     render() {
     return (
       <div className={classes.QuizList}>
         <div>
-            <h1>Список Тестов</h1>
-
-            <ul>
-                {this.renderQuizes()}
-            </ul>
+            <h1>Список Тестов</h1>            
+            {
+                this.state.loading
+                    ? <Loader />
+                    : <ul>
+                        {this.renderQuizes()}
+                    </ul>
+            }            
         </div>
         
       </div>
