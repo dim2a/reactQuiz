bee1054ff699f7eff6574669e13a40812ceaff40
dim2a <buinicki.dim@gmail.com>
2018-12-12 15:27:22 +0300

    Настроена авторизация через Firebase Auth REST API

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
+import axios from 'axios'
 
 
 export default class Auth extends PureComponent {
@@ -37,11 +38,32 @@ export default class Auth extends PureComponent {
     }
   }
 
-  loginHandler = () => {
-
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBH9lk9XqBBv35fUDM7PubDALiXuM83zk8', authData)
+      console.log(response.data)
+    } catch (e) {
+      console.log(e)
+    }
   }
 
-  registerHandler = () => {
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBH9lk9XqBBv35fUDM7PubDALiXuM83zk8', authData)
+      console.log(response.data)
+    } catch (e) {
+      console.log(e)
+    }
     
   }
 
