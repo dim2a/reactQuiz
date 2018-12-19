d19ee5b60e7c6c128c5e0ca1bfc2a383cc9cea6b
dim2a <buinicki.dim@gmail.com>
2018-12-19 10:02:50 +0300

    переделан под redux компонент создания теста

---
+++ src/store/actions/create.js
@@ -0,0 +1,22 @@
+import { CREATE_QUIT_QUESTION, RESET_QUIZ_CREATION } from "./actionTypes";
+import Axios from '../../axios/axios-quiz';
+
+export function createQuizQuestion(item) {
+    return {
+        type: CREATE_QUIT_QUESTION,
+        item
+    }
+}
+
+export function resetQuizCreation() {
+    return {
+        type: RESET_QUIZ_CREATION
+    }
+}
+
+export function finishCreateQuiz() {
+    return async (dispatch, getState) => {
+        await  Axios.post('/quizes.json', getState().create.quiz)
+        dispatch(resetQuizCreation)
+    }
+}
